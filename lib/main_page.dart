6330c07ae13290a65454e8427833c097ae332d8c
Sangin Han <hugedreamhd@gmail.com>
2023-12-19 23:57:27 +0900

    SingleChildScrollView - 스크롤막대생김

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -30,69 +30,71 @@ class _MainPageState extends State<MainPage> {
         title: Text('홈'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-            ),
-            SizedBox(height: 30),
-            Container(height: 30),
-            Text(
-              '$number',
-              style: TextStyle(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
                 color: Colors.red,
-                fontSize: 70,
+                width: 100,
+                height: 100,
               ),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print('ElevatedButton');
-              },
-              child: Text('ElevatedButton'),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                print('OutlinedButton');
-              },
-              child: Text('OutlinedButton'),
-            ),
-            TextButton(
-              onPressed: () {
-                print('TextButton');
-              },
-              child: Text('TextButton'),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: '글자', //여기
-                border: OutlineInputBorder(),
-              ),
-              onChanged: (text) {
-                print(text);
-              },
-            ),
-            Image.network(
-              'https://previews.123rf.com/images/sternfahrer/sternfahrer2304/sternfahrer230400828/202544978-%EB%A9%8B%EC%A7%84-%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B4%EC%97%90%EC%84%9C-%EC%98%81%EA%B0%90%EC%9D%84-%EB%B0%9B%EC%9D%80-%EB%B8%94%EB%9E%99%ED%99%80-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8%EB%A0%88%EC%9D%B4%EC%85%98-%ED%8C%90%ED%83%80%EC%A7%80-%EC%8A%A4%ED%83%80%EC%9D%BC-ai-%EC%83%9D%EC%84%B1.jpg',
-              width: 200,
-              height: 100,
-              fit: BoxFit.fill,
-            ),
-            Container(
-              color: Colors.red,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Image.asset(
-                    'assets/img/cat.jpg',
-                  width: 200,
-                  height: 100,
-                  fit: BoxFit.cover,
+              SizedBox(height: 100),
+              Container(height: 30),
+              Text(
+                '$number',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 70,
                 ),
               ),
-            ),
-          ],
+              ElevatedButton(
+                onPressed: () {
+                  print('ElevatedButton');
+                },
+                child: Text('ElevatedButton'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  print('OutlinedButton');
+                },
+                child: Text('OutlinedButton'),
+              ),
+              TextButton(
+                onPressed: () {
+                  print('TextButton');
+                },
+                child: Text('TextButton'),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: '글자', //여기
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (text) {
+                  print(text);
+                },
+              ),
+              Image.network(
+                'https://previews.123rf.com/images/sternfahrer/sternfahrer2304/sternfahrer230400828/202544978-%EB%A9%8B%EC%A7%84-%EB%B0%B0%EA%B2%BD%ED%99%94%EB%A9%B4%EC%97%90%EC%84%9C-%EC%98%81%EA%B0%90%EC%9D%84-%EB%B0%9B%EC%9D%80-%EB%B8%94%EB%9E%99%ED%99%80-%EC%9D%BC%EB%9F%AC%EC%8A%A4%ED%8A%B8%EB%A0%88%EC%9D%B4%EC%85%98-%ED%8C%90%ED%83%80%EC%A7%80-%EC%8A%A4%ED%83%80%EC%9D%BC-ai-%EC%83%9D%EC%84%B1.jpg',
+                width: 200,
+                height: 100,
+                fit: BoxFit.fill,
+              ),
+              Container(
+                color: Colors.red,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                      'assets/img/cat.jpg',
+                    width: 200,
+                    height: 100,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
