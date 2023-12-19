0b4758f0250981c470d0bb9c9c7bb1f1f6f82240
Sangin Han <hugedreamhd@gmail.com>
2023-12-20 00:31:57 +0900

    전역변수 지역변수

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -21,12 +21,14 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   //변수는 항상 소문자로 시작
   //변수 어디서나 변경가능
-  int number = 0;//변수
-  String _text = '';//변수
+  int number = 0;//전역변수
+  String _text = '';//아직 사용안된 전역변수
   final _textController = TextEditingController();//상수 더이상 변경이 불가능한 값 / 인스턴스 생성
 
   @override
   void dispose() {//화면이 닫힐때 or 더이상 사용이 안될때 호출
+    number = 11;//전역변수
+    int i = 11;//지역변수
     _textController.dispose();//메모리 해제
     super.dispose();
   }
