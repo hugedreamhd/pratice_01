9f6125b23cbff2898032d229786fda93d583b39c
Sangin Han <hugedreamhd@gmail.com>
2023-12-19 17:26:25 +0900

    MaterialApp? 머터리얼 디자인을 만들때 가장 위에 있어야 함
    ios라면 쿠퍼티노 디자인이 기본임
    
    primarySwatch 안되는데?

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+         primarySwatch: Colors.cyan,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
